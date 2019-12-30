a93b802191071dc2e2b7639656aa2bcc2ad5fb34
Richie Bendall <richiebendall@gmail.com>
2019-12-30 15:54:09 +1300

    refactor: General maintainence
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 "use strict"
 
-const BrowserFormData = require("formdata-polyfill")
-const NodeFormData = require("formdata-node")
-const envCrosser = require("env-crosser")
-
-module.exports = envCrosser({
-    browser: BrowserFormData,
-    node: NodeFormData,
-    worker: BrowserFormData,
-    fallback: undefined,
-})
+module.exports = require("formdata-node")
